f973a6741b009b81411d8bbcde7361bbcdb8f90f
ahaine <77943398+ahaine@users.noreply.github.com>
2021-04-30 14:45:08 -0400

    Updated my maps.html and js\script.js
    Fixed my map and added two markers to the map.  I am showing where I Grew up, the city of St. Joseph, MI.  One marker marks where I went to high school, and the other locates a local hotspot.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-function init(){
-	//alert('it works');
-	var el = document.getElementById('canvas');
-	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+function initMap(){
+	var map1 = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(42.089711, -86.491190);
 	var mapOptions = {
-		center: myLocation,
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
+			center: myLocation,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.roadmap,
+			mapTypeControlOptions: {
+				position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
 	};
 
-	var myMap = new google.maps.Map(el, mapOptions);
+	var myMap = new google.maps.Map(map1, mapOptions);
 	
 	var marker = new google.maps.Marker({
-      position: {lat: 53.98779, lng: 27.52468},
+      position: myLocation,
       map: myMap,
       animation: google.maps.Animation.DROP,
-      title: "Minsk"
-      
-  });
-
+      title: "St. Joseph High School"
+	});
 	
-
+	var marker = new google.maps.Marker({
+		position: {lat: 42.09888, lng: -86.48998},
+		map: myMap,
+		animation: google.maps.Animation.DROP,
+		title: "Dairy Korner"
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
